09f8d6b38849d31ffe35197a29c755dbc65e4ef6
ZYB <zyb26252@gmail.com>
2025-03-09 01:59:39 +0800

    fix: 修复 CORS 配置

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ except Exception as e:
 # 设置CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://aivul.love:3000", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
